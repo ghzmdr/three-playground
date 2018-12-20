168d817fa5613ed8625253833e6b668478cc7650
Vincent De Feo <vincent.defeo@caffeina.com>
2018-12-20 17:19:19 +0100

    [✨] Scene selection & rename

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# three-puncher
+# three-playground
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,30 @@
 import {qs} from '@okiba/dom'
-import Stage from '~/stages/stage-3'
 
+import Stage1 from '~/stages/stage-1'
+import Stage2 from '~/stages/stage-2'
+import Stage3 from '~/stages/stage-3'
+
+const form = qs('form')
+const stages = qs('#stages')
 const canvas = qs('#stage')
-const stage = new Stage(canvas)
 
-window.application = {
-  stage
+const stagesMap = {
+  Stage1,
+  Stage2,
+  Stage3
 }
+
+function init(Stage) {
+  form.parentNode.removeChild(form)
+  stages.parentNode.removeChild(stages)
+
+  window.application = {
+    stage: new Stage(canvas)
+  }
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  init(stagesMap[stages.value])
+})
+
